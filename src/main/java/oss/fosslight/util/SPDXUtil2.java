06e212e5865d7846d861686e460891c5f34d5787
riyenas0925 <riyenas0925@gmail.com>
2021-10-01 02:08:34 +0900

    Delete spdx-tools jar file and add spdx tools java dependency
    * before : Convert spdx files using spdx-tools jar in cmd
    * after : Add the spdx tools java dependency and convert the file using the SpdxConverter method
    * version used org.spdx:tools-java 1.0.2 (15-Jul-2021 release)
    * mavencentral : https://search.maven.org/artifact/org.spdx/tools-java/1.0.2/jar
    * github release : https://github.com/spdx/tools-java
    
    Signed-off-by: riyenas0925 <riyenas0925@gmail.com>

--- src/main/java/oss/fosslight/util/SPDXUtil2.java
+++ src/main/java/oss/fosslight/util/SPDXUtil2.java
@@ -10,23 +10,18 @@ import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import oss.fosslight.common.CommonFunction;
-import oss.fosslight.common.ShellCommander;
+import org.spdx.tools.SpdxConverter;
 
 public class SPDXUtil2 {
 	static final Logger logger = LoggerFactory.getLogger("DEFAULT_LOG");
-	static String ROOT_DIR = CommonFunction.getProperty("root.dir");
-	
+
 	public static void spreadsheetToRDF(String prjId, String spPath, String rdfPath) throws Exception {
 		// 기존 파일 변환 결과 파일이 존재하는 경우 삭제
 		File rdfFile = Paths.get(rdfPath).toFile();
 		rdfFile.deleteOnExit();
 		
 		logger.debug("spreadsheetToRDF ("+prjId+") :" + spPath + " => " + rdfPath);
-		String command = "java -jar " + ROOT_DIR + "/SPDX/spdx-tools-2.2.2-jar-with-dependencies.jar SpreadsheetToRDF " + spPath + " " + rdfPath;
-		logger.info("command : " + command);
-		ShellCommander.shellCommandWaitFor(command);
+		SpdxConverter.convert(spPath, rdfPath);
 	}
 	
 	public static void spreadsheetToTAG(String prjId, String spPath, String tagfPath) throws Exception {
@@ -35,8 +30,6 @@ public class SPDXUtil2 {
 		rdfFile.deleteOnExit();
 
 		logger.debug("SpreadsheetToTAG ("+prjId+") :" + spPath + " => " + tagfPath);
-		String command = "java -jar " + ROOT_DIR + "/SPDX/spdx-tools-2.2.2-jar-with-dependencies.jar SpreadsheetToTag " + spPath + " " + tagfPath;
-		logger.info("command : " + command);
-		ShellCommander.shellCommandWaitFor(command);
+		SpdxConverter.convert(spPath, tagfPath);
 	}
 }
