87cde084467f169452de3c902d40b60a0bc365ab
yugeeklab <yugeeklab@gmail.com>
2021-11-08 10:28:36 +0900

    Fix Xss issue in ckeditor
    Remove Source button in ckeditor.
    
    Signed-off-by: yugeeklab <yugeeklab@gmail.com>

--- src/main/resources/static/js/customEditorConf_Comment.js
+++ src/main/resources/static/js/customEditorConf_Comment.js
@@ -24,7 +24,7 @@ CKEDITOR.editorConfig = function(config) {
 	//config.toolbarGroups = [];
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Underline,Subscript,Superscript,Source';
 
 	// Set the most common block elements.
 	//config.format_tags = 'p;h1;h2;h3;pre';
