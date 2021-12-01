e4600cf0f7a2c8959245d5c84b442b9f1285a0b9
yugeeklab <yugeeklab@gmail.com>
2021-12-01 15:50:28 +0900

    Upgrade ckeditor4
    Upgrade ckeditor4 and Delete Source button.
    
    Signed-off-by: yugeeklab <yugeeklab@gmail.com>

--- src/main/resources/static/js/ckeditor/config.js
+++ src/main/resources/static/js/ckeditor/config.js
@@ -1,12 +1,12 @@
 /**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
@@ -22,34 +22,17 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
-		{ name: 'colors' }
-		//{ name: 'about' }
+		{ name: 'colors' },
+		{ name: 'about' }
 	];
-	
-	config.extraPlugins = 'autolink';
-	/*
-	config.extraPlugins = 'link';
-	config.extraPlugins = 'dialog';
-	config.extraPlugins = 'dialogui';
-	config.extraPlugins = 'fakeobjects';
-	*/
-	// preview를 추가하면 autolink가 동작하지 않은 
-	// TODO ckeditor 최신 버전으로 재구성 해야함
-	//config.extraPlugins = 'preview';
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Underline,Subscript,Superscript,Source';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-	
-	config.uploadUrl  = '/imageupload/upload2';
-	config.filebrowserUploadUrl = '/imageupload/upload';
-	config.image_previewText = 'In publishing and graphic design, lorem ipsum is placeholder text (filler text) commonly used to demonstrate the graphic elements of a document or visual presentation';
-	config.pasteFromWordRemoveFontStyles = false; 
-	config.pasteFromWordRemoveStyles = false;
 };
--- src/main/resources/static/js/ckeditor/plugins/imageuploader/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['username'])) {
-    exit;
-}
-
-session_destroy();
-header("Location: imgbrowser.php");
