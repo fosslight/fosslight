d77b8e97f4ca79d73e2ea5635b64be2b287e91dc
yugeeklab <yugeeklab@gmail.com>
2021-12-24 09:47:46 +0900

    Fix xss filter for BSD license
    Signed-off-by: yugeeklab <yugeeklab@gmail.com>

--- src/main/java/oss/fosslight/common/CustomXssFilter.java
+++ src/main/java/oss/fosslight/common/CustomXssFilter.java
@@ -8,28 +8,22 @@ package oss.fosslight.common;
 import java.util.List;
 
 import com.nhncorp.lucy.security.xss.XssPreventer;
+import com.nhncorp.lucy.security.xss.XssFilter;
+import com.nhncorp.lucy.security.xss.XssSaxFilter;
 
 import lombok.extern.slf4j.Slf4j;
 import oss.fosslight.domain.LicenseMaster;
 import oss.fosslight.domain.OssMaster;
 import oss.fosslight.domain.Project;
+import oss.fosslight.domain.PartnerMaster;
 
 @Slf4j
-public class XssFilter {
+public class CustomXssFilter {
+
     public static void licenseMasterFilter(List<LicenseMaster> list) {
         for(LicenseMaster licenseMaster : list) {
 
-            String escapedLicenseName = XssPreventer.escape(licenseMaster.getLicenseName());
-            String escapedLicenseType = XssPreventer.escape(licenseMaster.getLicenseType());
-            String escapedShortIdentifier = XssPreventer.escape(licenseMaster.getShortIdentifier());
-            String escapedDescription = XssPreventer.escape(licenseMaster.getDescription());
-            String escapedWebpage = XssPreventer.escape(licenseMaster.getWebpage());
-
-            licenseMaster.setLicenseName(escapedLicenseName);
-            licenseMaster.setLicenseType(escapedLicenseType);
-            licenseMaster.setShortIdentifier(escapedShortIdentifier);
-            licenseMaster.setDescription(escapedDescription);
-            licenseMaster.setWebpage(escapedWebpage);
+            licenseMasterFilter(licenseMaster);
         }
     }
 
@@ -40,7 +34,7 @@ public class XssFilter {
         String escapedDescription = XssPreventer.escape(licenseMaster.getDescription());
         String escapedWebpage = XssPreventer.escape(licenseMaster.getWebpage());
 
-        licenseMaster.setLicenseName(escapedLicenseName);
+        //licenseMaster.setLicenseName(escapedLicenseName);
         licenseMaster.setLicenseType(escapedLicenseType);
         licenseMaster.setShortIdentifier(escapedShortIdentifier);
         licenseMaster.setDescription(escapedDescription);
@@ -50,17 +44,7 @@ public class XssFilter {
     public static void ossMasterFilter(List<OssMaster> list) {
         for(OssMaster ossMaster : list) {
 
-            String escapedLicenseName = XssPreventer.escape(ossMaster.getLicenseName());
-            String escapedLicenseType = XssPreventer.escape(ossMaster.getLicenseType());
-            String escapedSummaryDescription = XssPreventer.escape(ossMaster.getSummaryDescription());
-            String escapedDownloadLocation = XssPreventer.escape(ossMaster.getDownloadLocation());
-            String escapedHomepage = XssPreventer.escape(ossMaster.getHomepage());
-
-            ossMaster.setLicenseName(escapedLicenseName);
-            ossMaster.setLicenseType(escapedLicenseType);
-            ossMaster.setSummaryDescription(escapedSummaryDescription);
-            ossMaster.setDownloadLocation(escapedDownloadLocation);
-            ossMaster.setHomepage(escapedHomepage);
+            ossMasterFilter(ossMaster);
         }
     }
 
@@ -81,9 +65,7 @@ public class XssFilter {
     public static void projectFilter(List<Project> list) {
         for(Project project : list) {
 
-            String escapedPrjName = XssPreventer.escape(project.getPrjName());
-
-            project.setPrjName(escapedPrjName);
+            projectFilter(project);
         }
     }
 
@@ -92,4 +74,26 @@ public class XssFilter {
 
         project.setPrjName(escapedName);
     }
+
+    public static void partnerFilter(List<PartnerMaster> list) {
+        for(PartnerMaster partner : list) {
+
+            partnerFilter(partner);
+        }
+    }
+
+    public static void partnerFilter(PartnerMaster partner) {
+        String escapedPartnerName = XssPreventer.escape(partner.getPartnerName());
+        String escapedSoftwareVersion = XssPreventer.escape(partner.getSoftwareVersion());
+        String escapedSoftwareName = XssPreventer.escape(partner.getSoftwareName());
+        String escapedDescription = XssPreventer.escape(partner.getDescription());
+        String escapedDivision = XssPreventer.escape(partner.getDivision());
+
+        partner.setPartnerName(escapedPartnerName);
+        partner.setSoftwareVersion(escapedSoftwareVersion);
+        partner.setSoftwareName(escapedSoftwareName);
+        partner.setDescription(escapedDescription);
+        partner.setDivision(escapedDivision);
+    }
 }
+
