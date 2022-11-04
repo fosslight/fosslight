cca6c92c7e53b52f5805fc9edd5e3289ee090d38
FOSSLight Developer <fosslight-dev@lge.com>
2022-11-04 12:29:50 +0900

    Fix nickname merge when loading auto analysis result

--- src/main/java/oss/fosslight/common/CommonFunction.java
+++ src/main/java/oss/fosslight/common/CommonFunction.java
@@ -3742,6 +3742,11 @@ public class CommonFunction extends CoTopComponent {
 					
 					if(newestOssInfo != null) {
 						changeAnalysisResultList.add(newestOssInfo); // seq 3 : 최신등록 정보
+						
+						if(newestOssInfo.getOssName().toUpperCase().equals(userData.getOssName().toUpperCase())
+								&& newestOssInfo.getOssNickname() != null) {
+							userData.setOssNickname(CommonFunction.mergeNickname(userData, newestOssInfo.getOssNickname()));
+						}
 					}
 					
 					changeAnalysisResultList.add(askalono);		 // seq 4 : askalono 정보
