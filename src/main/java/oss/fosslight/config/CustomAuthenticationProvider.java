818e6af8f8d6668ba14bb82048f78d04017c1bb7
min <min@minui-MacBookAir.local>
2022-12-07 14:31:59 +0900

    Change print log level in LDAP

--- src/main/java/oss/fosslight/config/CustomAuthenticationProvider.java
+++ src/main/java/oss/fosslight/config/CustomAuthenticationProvider.java
@@ -102,7 +102,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 				con = new InitialDirContext(properties);
 				rtnMap.put("isAuthenticated", true);
 			}catch (NamingException e) {
-				log.warn("LDAP NamingException userId : " + user_id + " ERROR Message :" + e.getMessage());
+				log.debug("LDAP NamingException userId : " + user_id + " ERROR Message :" + e.getMessage());
 				rtnMap.put("isAuthenticated", false);
 				return rtnMap;
 			} finally {
