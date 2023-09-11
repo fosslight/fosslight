100786cbb511c5327c65928c22556d82d5c4f8c8
FOSSLight Developer <fosslight-dev@lge.com>
2023-09-11 15:45:38 +0900

    Fix code management (delete newowrk server license list data)

--- migration/migration/scripts/20230818004358_update_v1.6.1.sql
+++ migration/migration/scripts/20230818004358_update_v1.6.1.sql
@@ -1,6 +1,9 @@
 -- // update v1.6.1
 -- Migration SQL that makes the change goes here.
 
+DELETE FROM `T2_CODE_DTL` WHERE `CD_NO` = "122";
+DELETE FROM `T2_CODE` WHERE `CD_NO` = "122";
+
 
 
 -- //@UNDO
