f334b73be2f46524eff91862565fd0aa303c3100
Min-Kyungsun <kyungsun.min@lge.com>
2023-09-15 17:45:21 +0900

    Update migration script

--- migration/migration/scripts/20230818004358_update_v1.6.1.sql
+++ migration/migration/scripts/20230818004358_update_v1.6.1.sql
@@ -3,8 +3,20 @@
 
 DELETE FROM `T2_CODE_DTL` WHERE `CD_NO` = "122";
 DELETE FROM `T2_CODE` WHERE `CD_NO` = "122";
-
-
+DELETE FROM `T2_CODE_DTL` WHERE `CD_NO` = "120" AND `CD_DTL_NO` = "16";
+INSERT INTO
+    `T2_CODE_DTL` (
+    `CD_NO`,
+    `CD_DTL_NO`,
+    `CD_DTL_NM`,
+    `CD_SUB_NO`,
+    `CD_DTL_EXP`,
+    `CD_ORDER`,
+    `USE_YN`
+)
+VALUES
+    ('120', '16', '프로젝트 PACKAGING FILE', '', 'zip,tar.gz,gz', 6, 'Y'),
+    ('903', '010', 'stackoverflow.com/revisions/', '', 'stackoverflow url', 10, 'Y');
 
 -- //@UNDO
 -- SQL to undo the change goes here.
