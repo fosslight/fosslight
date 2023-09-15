d0954ca804636e9a468f37cccd01f3ca7d2e0cc2
FOSSLight Developer <fosslight-dev@lge.com>
2023-09-15 19:25:37 +0900

    Add dependency tab of project identification

--- migration/migration/scripts/20230818004358_update_v1.6.1.sql
+++ migration/migration/scripts/20230818004358_update_v1.6.1.sql
@@ -18,6 +18,11 @@ VALUES
     ('120', '16', '프로젝트 PACKAGING FILE', '', 'zip,tar.gz,gz', 6, 'Y'),
     ('903', '010', 'stackoverflow.com/revisions/', '', 'stackoverflow url', 10, 'Y');
 
+ALTER TABLE `PROJECT_MASTER` ADD `IDENTIFICATION_SUB_STATUS_DEP` varchar(1) NULL DEFAULT NULL;
+ALTER TABLE `PROJECT_MASTER` ADD `DEP_CSV_FILE_ID` int(11) NULL DEFAULT NULL;
+ALTER TABLE `OSS_COMPONENTS` ADD `DEPENDENCIES` varchar(2000) NULL DEFAULT NULL;
+ALTER TABLE `OSS_COMPONENTS` ADD `REF_OSS_NAME` varchar(200) NULL DEFAULT NULL;
+
 -- //@UNDO
 -- SQL to undo the change goes here.
 
