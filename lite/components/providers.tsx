f97c87d0ed4eb94fbbad04afc012e66a94bde13f
최덕경 <hjcdg197@gmail.com>
2023-10-06 14:19:11 +0900

    [Add] Install `axios` and `react-query` packages

--- lite/components/providers.tsx
+++ lite/components/providers.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
+const queryClient = new QueryClient();
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <RecoilRoot>{children}</RecoilRoot>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>{children}</RecoilRoot>
+    </QueryClientProvider>
+  );
 }
