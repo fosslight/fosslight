02b61c3f332f6244e7cabe4ec5d26f39c2359f2e
최덕경 <hjcdg197@gmail.com>
2023-10-06 14:16:58 +0900

    [Add] Install `recoil` package

--- lite/app/layout.tsx
+++ lite/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from '@/components/providers';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
