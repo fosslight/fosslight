4a5a4c5e6ef96319a945b7c0334d4698e450192b
최덕경 <hjcdg197@gmail.com>
2023-10-06 16:49:53 +0900

    [Add] Install `SourceSansPro` font

--- lite/tailwind.config.ts
+++ lite/tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   plugins: []
 };
 export default config;
