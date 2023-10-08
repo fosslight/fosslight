3523cbfb287e369b2684c8ba04dfc7b93955a25d
최덕경 <hjcdg197@gmail.com>
2023-10-08 22:10:09 +0900

    [Add] Prepare color palette

--- lite/tailwind.config.ts
+++ lite/tailwind.config.ts
@@ -6,6 +6,19 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './lib/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  theme: {
+    extend: {
+      colors: {
+        charcol: '#34393f',
+        crimson: '#b02a42',
+        semiwhite: '#eeeeee',
+        semiblack: '#222222',
+        gray: '#bbbbbb',
+        semigray: '#cccccc',
+        darkgray: '#aaaaaa'
+      }
+    }
+  },
   plugins: []
 };
 export default config;
