058f55be4e0059b6f0a547a4b47895cf12f16bd9
최덕경 <hjcdg197@gmail.com>
2023-10-08 19:52:20 +0900

    [Add] Set default font

--- lite/app/layout.tsx
+++ lite/app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from '@/components/providers';
+import { sourceSansProFont } from '@/lib/fonts';
 import type { Metadata } from 'next';
 import './globals.css';
 
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={sourceSansProFont.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
