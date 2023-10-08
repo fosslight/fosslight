463decda44a5f0324c50a6d37975402508f61aa0
최덕경 <hjcdg197@gmail.com>
2023-10-08 22:16:43 +0900

    [Add] Implement UI of reset-password page

--- lite/app/(auth)/reset-password/page.tsx
+++ lite/app/(auth)/reset-password/page.tsx
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+
 export default function ResetPassword() {
-  return <div>Reset Password</div>;
+  return (
+    <form className="flex flex-col gap-y-6 p-6 bg-semiwhite rounded">
+      <div className="flex flex-col gap-y-1">
+        <div className="font-semibold">ID</div>
+        <input className="w-full py-1 bg-transparent border-b border-semigray outline-none" />
+      </div>
+      <div className="flex flex-col gap-y-1">
+        <div className="font-semibold">Email</div>
+        <input
+          className="w-full py-1 bg-transparent border-b border-semigray outline-none"
+          type="email"
+        />
+      </div>
+      <button className="w-full py-1 bg-crimson border border-crimson rounded text-lg text-semiwhite">
+        Reset Password
+      </button>
+      <Link className="mx-auto text-sm text-darkgray" href="/sign-in">
+        Return to sign in.
+      </Link>
+    </form>
+  );
 }
