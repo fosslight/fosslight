c5e97d8dc0c87c04c6b2c61bf74e922bffc5071e
최덕경 <hjcdg197@gmail.com>
2023-10-09 01:18:13 +0900

    [Add] Install `fontawesome` icons

--- lite/app/layout.tsx
+++ lite/app/layout.tsx
@@ -1,6 +1,7 @@
 import Providers from '@/components/providers';
 import { sourceSansProFont } from '@/lib/fonts';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${sourceSansProFont.className} text-semiblack`}>
         <Providers>{children}</Providers>
       </body>
+      <Script src="https://kit.fontawesome.com/b3681c51ce.js" />
     </html>
   );
 }
