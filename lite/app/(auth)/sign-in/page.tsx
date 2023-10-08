3167fb65f4368122466dd6108c6309112d186bf8
최덕경 <hjcdg197@gmail.com>
2023-10-08 22:16:28 +0900

    [Add] Implement UI of sign-in page

--- lite/app/(auth)/sign-in/page.tsx
+++ lite/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,44 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 export default function SignIn() {
-  return <div>Sign In</div>;
+  const router = useRouter();
+
+  return (
+    <form className="flex flex-col gap-y-6 p-6 bg-semiwhite rounded">
+      <div className="flex flex-col gap-y-1">
+        <div className="font-semibold">ID</div>
+        <input className="w-full py-1 bg-transparent border-b border-semigray outline-none" />
+      </div>
+      <div className="flex flex-col gap-y-1">
+        <div className="font-semibold">Password</div>
+        <input
+          className="w-full py-1 bg-transparent border-b border-semigray outline-none"
+          type="password"
+        />
+      </div>
+      <label className="flex items-center cursor-pointer">
+        <input className="w-4 h-4" type="checkbox" />
+        &ensp;
+        <span className="font-semibold">Save ID</span>
+      </label>
+      <div className="flex flex-col gap-y-2">
+        <button className="w-full py-1 bg-crimson border border-crimson rounded text-lg text-semiwhite">
+          Sign In
+        </button>
+        <button
+          className="w-full py-1 bg-transparent border border-gray rounded text-lg"
+          type="button"
+          onClick={() => router.push('/sign-up')}
+        >
+          Sign Up
+        </button>
+      </div>
+      <Link className="mx-auto text-sm text-darkgray" href="/reset-password">
+        Forgot password? Click here.
+      </Link>
+    </form>
+  );
 }
