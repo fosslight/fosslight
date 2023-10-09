f3399597ac0dabfe0fff5db610825f3da88f397b
최덕경 <hjcdg197@gmail.com>
2023-10-10 02:46:47 +0900

    [Add] Separate PC/mobile view

--- lite/app/(main)/layout.tsx
+++ lite/app/(main)/layout.tsx
@@ -2,11 +2,45 @@
 
 import FullSearchBar from '@/components/full-search-bar';
 import SideBar from '@/components/side-bar';
-import { useState } from 'react';
+import Logo from '@/public/images/logo.png';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [isSideBarShown, setIsSideBarShown] = useState(true);
+  const [view, setView] = useState<'pc' | 'mobile' | 'none'>('none');
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
+  useEffect(() => {
+    if (isMobile) setView('mobile');
+    else setView('pc');
+  }, [isMobile]);
+
+  // Wait until detecting appropriate view
+  if (view === 'none') return null;
+
+  // Mobile View (≤ 768px)
+  if (view === 'mobile') {
+    return (
+      <main className="flex flex-col min-h-screen">
+        <div className="sticky top-0 flex justify-between items-center h-12 px-4 bg-white shadow-[0_0_3px_2px_rgba(0,0,0,0.2)]">
+          <div className="relative w-6 h-6">
+            <Image src={Logo} fill sizes="48px" alt="fosslight" />
+          </div>
+          <div className="text-xl font-semibold">
+            FOSSLight Hub&nbsp;
+            <span className="text-lg font-light">Lite</span>
+          </div>
+          <i className="text-lg fa-solid fa-user"></i>
+        </div>
+        <div className="pt-4 px-4 pb-24">{children}</div>
+        <div className="fixed right-0 bottom-0 left-0 h-16 bg-charcol"></div>
+      </main>
+    );
+  }
+
+  // PC View (> 768px)
   return (
     <main className="flex min-h-screen">
       <SideBar isShown={isSideBarShown} />
