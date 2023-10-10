8a35108d9de985da5a2d7a57dbfa6f5f917f34b4
최덕경 <hjcdg197@gmail.com>
2023-10-10 13:02:08 +0900

    [Add] Implement menus in `BottomBar` component

--- lite/components/bottom-bar.tsx
+++ lite/components/bottom-bar.tsx
@@ -1,18 +1,91 @@
+'use client';
+
 import { menus, rootMenu } from '@/lib/literals';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const defaultIsMenuShown = Object.fromEntries(menus.map((menu) => [menu.name, false]));
 
 export default function BottomBar() {
+  const [isMenuShown, setIsMenuShown] = useState(defaultIsMenuShown);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    function handleClickOutsideSubMenu(e: MouseEvent) {
+      if (e.target && !(e.target as Element).closest('.menu')) {
+        setIsMenuShown({ ...defaultIsMenuShown });
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutsideSubMenu);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideSubMenu);
+    };
+  });
+
   return (
     <div className="fixed right-0 bottom-0 left-0 flex h-16 bg-charcol text-semiwhite">
-      <div className="flex-1 pt-2.5 text-center">
+      <Link
+        className={`flex-1 pt-2.5 ${pathname === '/' ? 'bg-semicharcol/20' : ''} text-center`}
+        href="/"
+      >
         <i className={`text-lg ${rootMenu.icon}`}></i>
         <div className="text-sm">{rootMenu.short}</div>
-      </div>
-      {menus.map((menu) => (
-        <div key={menu.name} className="flex-1 pt-2.5 text-center">
-          <i className={`text-lg ${menu.icon}`}></i>
-          <div className="text-sm">{menu.short}</div>
-        </div>
-      ))}
+      </Link>
+      {menus.map((menu) => {
+        if (!menu.sub) {
+          return (
+            <Link
+              key={menu.name}
+              className={`flex-1 pt-2.5 ${
+                pathname.startsWith(menu.path) ? 'bg-semicharcol/20' : ''
+              } text-center`}
+              href={menu.path}
+            >
+              <i className={`text-lg ${menu.icon}`}></i>
+              <div className="text-sm">{menu.short}</div>
+            </Link>
+          );
+        }
+
+        return (
+          <div
+            key={menu.name}
+            className={`menu relative flex-1 pt-2.5 ${
+              pathname.startsWith(menu.path) ? 'bg-semicharcol/20' : ''
+            } text-center cursor-pointer no-tap-highlight`}
+            onClick={() => {
+              const newIsMenuShown = {
+                ...defaultIsMenuShown,
+                [menu.name]: !isMenuShown[menu.name]
+              };
+              setIsMenuShown(newIsMenuShown);
+            }}
+          >
+            <i className={`text-lg ${menu.icon}`}></i>
+            <div className="text-sm">{menu.short}</div>
+            <div
+              className={`absolute right-0 bottom-16 left-0 flex flex-col bg-charcol border-b border-darkgray rounded-t-md text-semiwhite ${
+                !isMenuShown[menu.name] ? 'max-h-0 opacity-0' : 'max-h-40 opacity-100'
+              } overflow-y-hidden transition-[max-height,opacity] duration-[500ms] z-50`}
+            >
+              {menu.sub.map((subMenu) => (
+                <Link
+                  key={subMenu.name}
+                  className={`${
+                    pathname.startsWith(menu.path + subMenu.path) ? 'bg-semicharcol/20' : ''
+                  } leading-9`}
+                  href={menu.path + subMenu.path}
+                >
+                  {subMenu.short}
+                </Link>
+              ))}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
