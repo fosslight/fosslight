5fed49796e7ed858d5e091bcfd8f94142eba35bc
최덕경 <hjcdg197@gmail.com>
2023-10-12 16:50:49 +0900

    [Add] Pick `OSSList` page as template example

--- lite/app/(main)/database/oss/page.tsx
+++ lite/app/(main)/database/oss/page.tsx
@@ -1,3 +1,26 @@
+import ListFilters from '@/components/list-filters';
+import ListTable from '@/components/list-table';
+
 export default function OSSList() {
-  return <div>Database &gt; OSS List</div>;
+  return (
+    <>
+      {/* Breadcrumb */}
+      <h2 className="pb-2 text-xl font-black">
+        Database
+        <i className="mx-2 text-sm fa-solid fa-angle-right"></i>
+        OSS List
+      </h2>
+
+      {/* Description */}
+      <h3 className="pb-8">
+        List of OSS(= Open Source Software) information registered in the database.
+      </h3>
+
+      {/* Filters */}
+      <ListFilters />
+
+      {/* Table */}
+      <ListTable />
+    </>
+  );
 }
