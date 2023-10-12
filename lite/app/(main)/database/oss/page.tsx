c1e2565c42a980746ba06bf5beadafca44ba5688
최덕경 <hjcdg197@gmail.com>
2023-10-12 18:53:07 +0900

    [Add] Implement UI of button container

--- lite/app/(main)/database/oss/page.tsx
+++ lite/app/(main)/database/oss/page.tsx
@@ -1,5 +1,7 @@
 import ListFilters from '@/components/list-filters';
 import ListTable from '@/components/list-table';
+import ExcelIcon from '@/public/images/excel.png';
+import Image from 'next/image';
 
 export default function OSSList() {
   return (
@@ -18,6 +20,16 @@ export default function OSSList() {
 
       {/* Filters */}
       <ListFilters />
+
+      {/* Button(s) */}
+      <div className="flex justify-end gap-x-2 mt-8">
+        <button className="flex items-center gap-x-1.5 px-2 py-0.5 border border-gray rounded">
+          <div className="relative w-4 h-4">
+            <Image src={ExcelIcon} fill sizes="32px" alt="export" />
+          </div>
+          Export
+        </button>
+      </div>
 
       {/* Table */}
       <ListTable />
