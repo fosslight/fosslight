a80d22c8aea740bcd90d78ec7c3ced495bec743f
최덕경 <hjcdg197@gmail.com>
2023-10-20 17:53:07 +0900

    [Add] Implement UI of oss part in dashboard page

--- lite/app/(main)/page.tsx
+++ lite/app/(main)/page.tsx
@@ -9,6 +9,7 @@ import { useSetRecoilState } from 'recoil';
 export default function Dashboard() {
   const setLoading = useSetRecoilState(loadingState);
   const [vulnerabilityList, setVulnerabilityList] = useState<any[]>([]);
+  const [ossList, setOssList] = useState<any[]>([]);
 
   const sectionHeaderClass = 'pl-2 mb-4 border-l-4 border-l-semiblack font-bold leading-tight';
   const sectionClass =
@@ -28,6 +29,18 @@ export default function Dashboard() {
           summary: 'A flaw was found in cairo image-compositor.c in all versions prior to 1.17.4.',
           published: '2021-03-18 23:54:08.0',
           modified: '2023-05-03 21:32:05.0'
+        }))
+      );
+
+      setOssList(
+        Array.from(Array(3)).map(() => ({
+          ossName: 'cairo',
+          ossVersion: '1.4.12',
+          licenseName: '(MPL-1.1 AND GPL-2.0) OR (LGPL-2.1 AND GPL-2.0)',
+          obligations: 'YY',
+          cvssScore: '7.8',
+          created: '2023-10-05 23:54:08.0',
+          modified: '2023-10-07 21:32:05.0'
         }))
       );
 
@@ -97,7 +110,51 @@ export default function Dashboard() {
             )}
           </div>
         </div>
-        <div className={clsx('col-span-2 lg:col-span-1', sectionClass)}>...</div>
+        <div className={clsx('col-span-2 lg:col-span-1', sectionClass)}>
+          <div className="flex items-center gap-x-3 text-sm">
+            <div className="px-2 py-0.5 bg-charcoal rounded text-semiwhite">OSS</div>
+            <Link className="text-charcoal" href="/database/oss?s=mod-dsc">
+              show more here
+            </Link>
+          </div>
+          <div className="flex flex-col gap-y-3 mt-4">
+            {ossList.length > 0 ? (
+              ossList.map((oss, idx) => (
+                <div
+                  key={idx}
+                  className="flex gap-x-3 pb-3 border-b border-b-semigray last:pb-0 last:border-none"
+                >
+                  <div className="flex flex-col gap-y-1">
+                    <div className="flex gap-x-2 items-center">
+                      <div className="line-clamp-1 font-semibold">{oss.ossName}</div>
+                      <div className="flex-shrink-0 font-semibold">({oss.ossVersion})</div>
+                      <div className="flex items-center gap-x-1 flex-shrink-0 px-1 py-1 border border-darkgray rounded text-xs">
+                        {oss.obligations[0] === 'Y' && (
+                          <i className="fa-solid fa-file-lines" title="Notice"></i>
+                        )}
+                        {oss.obligations[1] === 'Y' && (
+                          <i className="fa-solid fa-code" title="Source"></i>
+                        )}
+                      </div>
+                      <div className="flex-shrink-0 px-1 py-0.5 border border-orange-500 rounded text-xs">
+                        <span className="text-orange-500">{oss.cvssScore}</span>
+                      </div>
+                    </div>
+                    <div className="line-clamp-3 text-sm text-semiblack/80">{oss.licenseName}</div>
+                    <div className="text-sm text-darkgray">
+                      <i className="fa-solid fa-check"></i>&ensp;
+                      {oss.created < oss.modified
+                        ? `${oss.modified.substring(0, 10)} modified`
+                        : `${oss.created.substring(0, 10)} created`}
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="text-darkgray">No entries</div>
+            )}
+          </div>
+        </div>
         <div className={clsx('col-span-2 lg:col-span-1', sectionClass)}>...</div>
       </div>
     </>
