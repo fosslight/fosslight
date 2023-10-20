32d39bcae03aa470cd43590e19ef43fb57f1ef07
최덕경 <hjcdg197@gmail.com>
2023-10-20 22:35:06 +0900

    [Add] Link website logo to the dashboard page

--- lite/components/top-bar.tsx
+++ lite/components/top-bar.tsx
@@ -1,16 +1,17 @@
 import Logo from '@/public/images/logo.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function TopBar() {
   return (
     <div className="flex justify-between items-center h-12 px-4">
-      <div className="relative w-6 h-6">
+      <Link className="relative w-6 h-6 no-tap-highlight" href="/">
         <Image src={Logo} fill sizes="48px" alt="fosslight" />
-      </div>
-      <div className="text-xl font-semibold">
+      </Link>
+      <Link className="text-xl font-semibold no-tap-highlight" href="/">
         FOSSLight Hub&nbsp;
         <span className="text-lg font-light">Lite</span>
-      </div>
+      </Link>
       <i className="text-lg fa-solid fa-user"></i>
     </div>
   );
