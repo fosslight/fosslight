ef5e79a8185950c28d7c7270bcabaa60d503f8fd
FOSSLight Developer <fosslight-dev@lge.com>
2023-10-23 11:45:58 +0900

    Fix maintain admin check when paging BOM tab

--- src/main/java/oss/fosslight/service/impl/ProjectServiceImpl.java
+++ src/main/java/oss/fosslight/service/impl/ProjectServiceImpl.java
@@ -2810,8 +2810,12 @@ public class ProjectServiceImpl extends CoTopComponent implements ProjectService
 		}
 		identification.setOssVersionEmptyFlag(null);
 		
+		List<String> adminCheckRefComponentIdList = new ArrayList<>();
+		
 		if (bomList != null && !bomList.isEmpty()) {
 			for (ProjectIdentification pi : bomList) {
+				if (pi.getAdminCheckYn().equals(CoConstDef.FLAG_YES)) adminCheckRefComponentIdList.add(pi.getRefComponentId());
+				
 				// convert max score
 				if (pi.getCvssScoreMax() != null) {
 					cvssScoreMaxList.add(pi.getCvssScoreMax());
@@ -2861,7 +2865,11 @@ public class ProjectServiceImpl extends CoTopComponent implements ProjectService
 				bean.setRefDiv(bean.getReferenceDiv());
 				bean.setReferenceDiv(CoConstDef.CD_DTL_COMPONENT_ID_BOM);
 				bean.setRefComponentId(bean.getComponentId());
-				bean.setAdminCheckYn(CoConstDef.FLAG_NO);
+				if (adminCheckRefComponentIdList.contains(bean.getRefComponentId())) {
+					bean.setAdminCheckYn(CoConstDef.FLAG_YES);
+				} else {
+					bean.setAdminCheckYn(CoConstDef.FLAG_NO);
+				}
 				bean.setPreObligationType(bean.getObligationType());
 				
 				// 그리드 데이터 넣기
