d5b59910da59b191203e79a7eff481054a1e1e87
FOSSLight Developer <fosslight-dev@lge.com>
2023-10-25 18:15:12 +0900

    Fix version of latest registration info to analysis result version

--- src/main/java/oss/fosslight/common/CommonFunction.java
+++ src/main/java/oss/fosslight/common/CommonFunction.java
@@ -3954,7 +3954,7 @@ public static String makeRecommendedLicenseString(OssMaster ossmaster, ProjectId
 						newestOssInfo = ossService.getNewestOssInfo(userData); // 사용자 정보의 ossName기준 최신 등록정보
 						if (newestOssInfo != null) {
 							newestOssInfo.setGridId(""+gridSeq++);
-							newestOssInfo.setOssVersion(bean.getOssVersion());
+							newestOssInfo.setOssVersion(!isEmpty(bean.getOssVersion()) ? bean.getOssVersion() : userData.getOssVersion());
 							newestOssInfo.setComment(comment);
 						}
 						
@@ -4044,7 +4044,7 @@ public static String makeRecommendedLicenseString(OssMaster ossmaster, ProjectId
 					
 					if (totalNewestOssInfo != null) {
 						totalNewestOssInfo.setGridId(""+gridSeq++);
-						totalNewestOssInfo.setOssVersion(bean.getOssVersion());
+						totalNewestOssInfo.setOssVersion(!isEmpty(bean.getOssVersion()) ? bean.getOssVersion() : userData.getOssVersion());
 						totalNewestOssInfo.setComment(comment);
 						
 						String totalNewestMergeNickName = CommonFunction.mergeNickname(totalAnalysis, totalNewestOssInfo.getOssNickname()); // 사용자 작성 정보 & 최신등록정보 nickname Merge
@@ -4114,7 +4114,7 @@ public static String makeRecommendedLicenseString(OssMaster ossmaster, ProjectId
 						OssAnalysis newestOssInfo = ossService.getNewestOssInfo(userData); // 사용자 정보의 ossName기준 최신 등록정보
 						
 						if (newestOssInfo != null && !deactivateOssList.contains(newestOssInfo.getOssName().toUpperCase())) {
-							newestOssInfo.setOssVersion(bean.getOssVersion());
+							newestOssInfo.setOssVersion(!isEmpty(bean.getOssVersion()) ? bean.getOssVersion() : userData.getOssVersion());
 							newestOssInfo.setGridId(""+gridSeq++);
 							
 							changeAnalysisResultList.add(newestOssInfo); // seq 2 : 최신등록 정보
