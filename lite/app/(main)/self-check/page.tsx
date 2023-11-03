65382f1321f873b12e7d3ac46a22c4489cdd7a85
최덕경 <hjcdg197@gmail.com>
2023-11-03 17:30:07 +0900

    [Improve] Refactor by `SelfCheckModal` component

--- lite/app/(main)/self-check/page.tsx
+++ lite/app/(main)/self-check/page.tsx
@@ -2,7 +2,7 @@
 
 import ListFilters from '@/components/list-filters';
 import ListTable from '@/components/list-table';
-import Modal from '@/components/modal';
+import SelfCheckModal from '@/components/self-check-modal';
 import { loadingState } from '@/lib/atoms';
 import { parseFilters } from '@/lib/filters';
 import ExcelIcon from '@/public/images/excel.png';
@@ -113,42 +113,12 @@ export default function SelfCheckList() {
           <i className="text-sm fa-solid fa-plus" /> Create Project
         </button>
       </div>
-      <Modal show={isModalShown} onHide={() => setIsModalShown(false)} hideByBackdrop={false}>
-        <div className="pb-4 mb-4 border-b border-b-semigray font-bold">
-          <i className="text-sm fa-solid fa-plus"></i>&ensp;Create a project to check
-        </div>
-        <div className="flex flex-col gap-y-4 w-72">
-          <div className="flex flex-col gap-y-2">
-            <label className="text-sm font-semibold">
-              Project Name <span className="font-bold text-crimson">*</span>
-            </label>
-            <input
-              className="w-full px-2 py-1 border border-darkgray outline-none"
-              placeholder="EX) FOSSLight Hub Lite"
-            />
-          </div>
-          <div className="flex flex-col gap-y-2">
-            <label className="text-sm font-semibold">Project Version</label>
-            <input
-              className="w-full px-2 py-1 border border-darkgray outline-none"
-              placeholder="EX) 1.0.0"
-            />
-          </div>
-          <div className="flex flex-col gap-y-2">
-            <label className="text-sm font-semibold">Comment</label>
-            <textarea
-              className="w-full px-2 py-1 border border-darkgray outline-none resize-none"
-              rows={3}
-            ></textarea>
-          </div>
-        </div>
-        <div className="flex justify-end gap-x-1 mt-4">
-          <button className="px-2 py-0.5 crimson-btn">Create</button>
-          <button className="px-2 py-0.5 default-btn" onClick={() => setIsModalShown(false)}>
-            Cancel
-          </button>
-        </div>
-      </Modal>
+      <SelfCheckModal
+        mode="create"
+        data={{ name: '', version: '', comment: '' }}
+        show={isModalShown}
+        onHide={() => setIsModalShown(false)}
+      />
 
       {/* Table (Rows/Columns + Sorting + Pagination) */}
       <ListTable
