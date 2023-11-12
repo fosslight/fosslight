075bbe1819aa301f7a7a928127114de6074a3410
cobaltblu27 <cobaltblu27@gmail.com>
2023-11-12 22:38:32 +0900

    [Add] Implement mapping logics for fixing pagination errors

--- src/main/java/oss/fosslight/api/dto/Paging.java
+++ src/main/java/oss/fosslight/api/dto/Paging.java
@@ -1,5 +1,6 @@
 package oss.fosslight.api.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,7 @@ import lombok.Setter;
 @Setter
 public class Paging {
     public enum SortDirection {
-        ASC("asc"), DESC("desc");
+        ASC("asc"), DSC("desc");
 
         SortDirection(String value) {
             this.value = value;
@@ -18,10 +19,21 @@ public class Paging {
 
     protected int page;
     protected int countPerPage;
-    protected SortDirection sort;
+    protected SortDirection sortDirection;
     protected String sortColumn;
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    protected String sort;
 
     public int getOffset() {
         return (page - 1) * countPerPage;
+    }
+
+    public void setSort(String sort) {
+        if (sort == null || sort.isEmpty() || !sort.contains("-")) return;
+        var sortSplit = sort.toUpperCase().split("-");
+        this.sortColumn = sortSplit[0].toUpperCase();
+        this.sortDirection = SortDirection.valueOf(sortSplit[1]);
     }
 }
