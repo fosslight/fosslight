362fd9c8af7b07d9790f5dd85bdedffb1b618314
최덕경 <hjcdg197@gmail.com>
2023-12-08 18:54:29 +0900

    [Improve] Fine-tune the code

--- lite/lib/literals.ts
+++ lite/lib/literals.ts
@@ -1,3 +1,21 @@
+export const OSS_TYPES: Common.OSSTypes = {
+  M: {
+    name: 'Multi License',
+    desc: 'It contains source codes under multiple licenses. (EX. lib is LGPL-2.1 and src is GPL-2.0)',
+    color: '#8487bc'
+  },
+  D: {
+    name: 'Dual License',
+    desc: 'You can select one of the registered licenses. (EX. GPL-2.0 or MIT)',
+    color: '#cc8fbb'
+  },
+  V: {
+    name: 'Version Different License',
+    desc: 'It is distributed under different licenses according to its versions. (EX. 1.0 is GPL-2.0, but 2.0 is BSD-3-Clause)',
+    color: '#ddae91'
+  }
+};
+
 export const RESTRICTIONS = [
   ['1', 'Non-commercial Use Only'],
   ['2', 'Network Copyleft'],
@@ -51,21 +69,3 @@ export const SELF_CHECK_TABS: SelfCheck.Tab[] = [
       'Generate a notice document explaining the OSS in your project, as a format that you want.'
   }
 ];
-
-export const OSS_TYPES: Detail.OSSTypes = {
-  M: {
-    name: 'Multi License',
-    desc: 'It contains source codes under multiple licenses. (EX. lib is LGPL-2.1 and src is GPL-2.0)',
-    color: '#8487bc'
-  },
-  D: {
-    name: 'Dual License',
-    desc: 'You can select one of the registered licenses. (EX. GPL-2.0 or MIT)',
-    color: '#cc8fbb'
-  },
-  V: {
-    name: 'Version Different License',
-    desc: 'It is distributed under different licenses according to its versions. (EX. 1.0 is GPL-2.0, but 2.0 is BSD-3-Clause)',
-    color: '#ddae91'
-  }
-};
