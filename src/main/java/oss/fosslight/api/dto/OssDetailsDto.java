cf5285f1961195e1acd16ff88e6e363942e38f61
cobaltblu27 <cobaltblu27@gmail.com>
2023-12-20 01:54:48 +0900

    [Improve] Fix inversed deactivate flag

--- src/main/java/oss/fosslight/api/dto/OssDetailsDto.java
+++ src/main/java/oss/fosslight/api/dto/OssDetailsDto.java
@@ -14,6 +14,6 @@ public class OssDetailsDto extends OssDto {
     Boolean deactivate;
 
     public void setDeactivate(String deactivateFlag) {
-        deactivate = !"Y".equals(deactivateFlag);
+        deactivate = "Y".equals(deactivateFlag);
     }
 }
