2f9f65758a4d6e9e19690d50b94fe9878fd75dc5
cobaltblu27 <cobaltblu27@gmail.com>
2023-12-20 02:51:49 +0900

    [Add] implement endpoint for user/divisions

--- src/main/java/oss/fosslight/api/controller/lite/LiteUserController.java
+++ src/main/java/oss/fosslight/api/controller/lite/LiteUserController.java
@@ -4,16 +4,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import oss.fosslight.api.dto.GetUserInfoDto;
-import oss.fosslight.api.dto.ListVulnerabilityDto;
+import oss.fosslight.api.dto.UserDto;
+import oss.fosslight.common.CoCodeManager;
+import oss.fosslight.common.CoConstDef;
 import oss.fosslight.common.Url;
 import oss.fosslight.domain.T2Users;
+import oss.fosslight.repository.ApiCodeMapper;
 import oss.fosslight.service.ApiVulnerabilityService;
 import oss.fosslight.service.T2UserService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,19 +33,41 @@ public class LiteUserController {
     @Autowired
     T2UserService userService;
 
+    @Autowired
+    ApiCodeMapper codeMapper;
+
     @GetMapping("/me")
     public @ResponseBody ResponseEntity<GetUserInfoDto.Result> getMeInfo() {
         try {
-            SecurityContext sec = SecurityContextHolder.getContext();
-            AbstractAuthenticationToken auth = (AbstractAuthenticationToken)sec.getAuthentication();
-            T2Users user = new T2Users();
-            user.setUserId(auth.getName());
-            T2Users userInfo = userService.getUserAndAuthorities(user);
+            var userInfo = userService.getLoginUserInfo();
             var result = GetUserInfoDto.Result.builder()
                     .username(userInfo.getUserName())
                     .email(userInfo.getEmail())
                     .build();
             return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/users")
+    public @ResponseBody ResponseEntity<List<UserDto>> getUserList() {
+        try {
+            var users = userService.getAllUsers(new T2Users())
+                    .stream().map(UserDto::new).collect(Collectors.toList());
+            return ResponseEntity.ok(users);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/divisions")
+    public @ResponseBody ResponseEntity<List<Map<String, Object>>> getDivisions() {
+        try {
+            var codes = codeMapper.selectCodeList(CoConstDef.CD_USER_DIVISION, null);
+            return ResponseEntity.ok(codes);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             return ResponseEntity.internalServerError().build();
