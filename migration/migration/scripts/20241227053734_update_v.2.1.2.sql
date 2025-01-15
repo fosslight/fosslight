f138ac7d0164bd2e8e361fcf4d0976dc6648528e
FOSSLight Developer <fosslight-dev@lge.com>
2025-01-15 14:14:37 +0900

    Fix function to send email when comment is changed

--- migration/migration/scripts/20241227053734_update_v.2.1.2.sql
+++ migration/migration/scripts/20241227053734_update_v.2.1.2.sql
@@ -2,3 +2,23 @@ ALTER TABLE `PROJECT_MASTER` ADD `VULDOC_SKIP_YN` char(1) DEFAULT 'N';
 
 INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('214', '22', 'Identification', '', '', 10, 'Y');
 INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('214', '23', 'Identification temporary save comments', '', '', 11, 'Y');
+
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '132', '[FOSSLight][OSS-${OSS ID}] ${User} deleted comment on : "${OSS Name}"', '', '', 132, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '233', '[FOSSLight][LIC-${License ID}] ${User} deleted comment on : "${License Name}"', '', '', 233, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '341', '[FOSSLight][PRJ-${Project ID}] ${User} deleted comment on : "${Project Name}"', '', '', 341, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '431', '[FOSSLight][PRJ-${Project ID}] Identification, ${User} deleted comment on : "${Project Name}"', '', '', 431, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '531', '[FOSSLight][PRJ-${Project ID}] Packaging, ${User} deleted comment on : "${Project Name}"', '', '', 531, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '651', '[FOSSLight][PRJ-${Project ID}] Distribution, ${User} deleted comment on : "${Project Name}"', '', '', 651, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '741', '[FOSSLight][3rd-${3rd Party ID}] ${User} deleted comment on : "${3rd Party Name}"', '', '', 741, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '742', '[FOSSLight][3rd-${3rd Party ID}] Identification, ${User} modified comment on : "${3rd Party Name}"', '', '', 742, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('102', '743', '[FOSSLight][3rd-${3rd Party ID}] Identification, ${User} deleted comment on : "${3rd Party Name}"', '', '', 743, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '132', 'oss comment 삭제', '', '100', 132, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '233', 'license comment 삭제', '', '101', 233, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '341', 'Project BasicInformation에서 Comment 삭제', '', '200', 341, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '431', 'Identification comment 삭제', '', '200', 431, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '531', 'Packaging comment 삭제', '', '200', 531, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '651', 'Distribution comment 삭제', '', '200', 651, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '741', '3rd comment 삭제', '', '205', 741, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '742', '3rd Identification comment 수정', '', '205', 742, 'Y');
+INSERT INTO `T2_CODE_DTL` (`CD_NO`, `CD_DTL_NO`, `CD_DTL_NM`, `CD_SUB_NO`, `CD_DTL_EXP`, `CD_ORDER`, `USE_YN`) VALUES ('103', '743', '3rd Identification comment 삭제', '', '205', 743, 'Y');
+UPDATE `T2_CODE_DTL` SET CD_DTL_EXP = '130,132,230,233,340,341,430,431,530,531,650,651,740,741,742,743' WHERE CD_NO = '110' AND CD_DTL_NO = '36';
