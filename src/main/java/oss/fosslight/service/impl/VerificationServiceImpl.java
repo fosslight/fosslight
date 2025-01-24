338831c7a0e675800e3d467c7324d73c97b0e2e2
FOSSLight Developer <fosslight-dev@lge.com>
2025-01-24 15:53:00 +0900

    Fix banned list file creation function

--- src/main/java/oss/fosslight/service/impl/VerificationServiceImpl.java
+++ src/main/java/oss/fosslight/service/impl/VerificationServiceImpl.java
@@ -1348,6 +1348,7 @@ public class VerificationServiceImpl extends CoTopComponent implements Verificat
 			//STEP 8 : Verify 동작 후 Except File Result DB 저장
 			log.info("VERIFY Read exceptFileContent file START -----------------");
 			
+			String existExceptFileContent = CommonFunction.getStringFromFile(VERIFY_PATH_OUTPUT +"/"+prjId+"/except_file_result");
 			exceptFileContent = CommonFunction.getStringFromFile(VERIFY_PATH_OUTPUT +"/"+prjId+"/except_file_result", VERIFY_PATH_DECOMP +"/" + prjId +"/", checkExceptionWordsList, checkExceptionIgnoreWorksList);
 			
 			log.info("VERIFY Read exceptFileContent file END -----------------");
@@ -1365,7 +1366,10 @@ public class VerificationServiceImpl extends CoTopComponent implements Verificat
 			// 서버 디렉토리를 replace한 내용으로 새로운 파일로 다시 쓴다.
 			if (!isEmpty(exceptFileContent)) {
 				log.info("VERIFY writeFile exceptFileContent file START -----------------");
-				
+				if (!isEmpty(existExceptFileContent)) {
+					existExceptFileContent += System.lineSeparator() + exceptFileContent;
+					exceptFileContent = existExceptFileContent;
+				}
 				FileUtil.writeFile(VERIFY_PATH_OUTPUT +"/" + prjId, CoConstDef.PACKAGING_VERIFY_FILENAME_PROPRIETARY, exceptFileContent.replaceAll(VERIFY_PATH_DECOMP +"/" + prjId +"/", ""));
 				
 				log.info("VERIFY writeFile exceptFileContent file END -----------------");
