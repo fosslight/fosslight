7b907faba6c6ada9eded7a6d817b68bb67edcbef
FOSSLight Developer <fosslight-dev@lge.com>
2026-02-27 17:19:22 +0900

    Fix identification BOM storage function

--- src/main/java/oss/fosslight/repository/ProjectMapper.java
+++ src/main/java/oss/fosslight/repository/ProjectMapper.java
@@ -383,8 +383,6 @@ public interface ProjectMapper {
 
 	public List<OssComponents> selectOssComponentsListClassAppend(ProjectIdentification identification);
 
-	List<OssComponents> selectVulnerabilityResolutionSecurityList(Project project);
-
 	int copySecurityDataForProjectCnt(Project project);
 
 	void copySecurityDataForProject(Project project);
