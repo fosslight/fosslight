24cf9e4526ba7d2b07315c245bc058c59986209f
hyeinlee <hyein.lee.work@gmail.com>
2026-09-22 17:26:34 +0900

    fix: change template file name
    modify file name SPDXRdf_2.2.2.xlsx to SPDXRdf_2.3.xlsx

--- src/main/java/oss/fosslight/util/ExcelDownLoadUtil.java
+++ src/main/java/oss/fosslight/util/ExcelDownLoadUtil.java
@@ -2680,7 +2680,7 @@ public class ExcelDownLoadUtil extends CoTopComponent {
 		String prjId = ossNotice.getPrjId();
 
 		try {
-			inFile= new FileInputStream(new File(downloadpath+"/SPDXRdf_2.2.2.xls"));
+			inFile= new FileInputStream(new File(downloadpath+"/SPDXRdf_2.3.xlsx"));
 
 			wb = WorkbookFactory.create(inFile);
 			sheetDoc = wb.getSheetAt(0);
@@ -3307,7 +3307,7 @@ public class ExcelDownLoadUtil extends CoTopComponent {
 		ossNotice.setFileType("text");
 
 		try {
-			inFile= new FileInputStream(new File(downloadpath+"/SPDXRdf_2.2.2.xls"));
+			inFile= new FileInputStream(new File(downloadpath+"/SPDXRdf_2.3.xlsx"));
 
 			wb = WorkbookFactory.create(inFile);
 			sheetDoc = wb.getSheetAt(0);
@@ -4025,7 +4025,7 @@ public class ExcelDownLoadUtil extends CoTopComponent {
 		String prjId = ossNotice.getPrjId();
 		
 		try {
-			inFile= new FileInputStream(new File(downloadpath+"/SPDXRdf_2.2.2.xls"));
+			inFile= new FileInputStream(new File(downloadpath+"/SPDXRdf_2.3.xlsx"));
 			
 			wb = WorkbookFactory.create(inFile);
 			sheetDoc = wb.getSheetAt(0);
